1e7116bca2bd4ce636788b73c4d7fa89ee97175d
Ben Pang <benjinpang@gmail.com>
2016-07-25 08:14:45 -0700

    truncate stuff

--- README.md
+++ README.md
@@ -40,3 +40,4 @@ Webview:
 
 ##Todo
 - Adblock
+- History?
--- renderer.js
+++ renderer.js
@@ -38,60 +38,81 @@ onload=_=>{
     document.body.appendChild(x)
     //add new tab
     i.innerHTML+=`<li><a>${v.getTitle()}</a></li>`
+    Array.from(document.querySelectorAll('#i li a')).map(x=>x.style.maxWidth=(0|100/document.querySelectorAll('#i li a').length-5)+'vw')
   }
+
+  //this triggers on _target='blank' links and stuff like that
+  v.addEventListener('new-window',e=>{
+    nV()
+    v=V()[C=V().length-1]
+    v.src=e.url
+    v.addEventListener('did-finish-load',uT)
+  })
 
   //key commands
   s.onkeypress=e=>{
     //all commands are triggered by the enter key
     if(e.which==13){
-      e.preventDefault()
+      e.preventDefault();
 
-      //devtools
-      if(s.value.match(/^d */))
-        v.openDevTools();
-      //go back
-      else if(s.value.match(/^< */))
-        v.goBack();
-      //go forward
-      else if(s.value.match(/^> */))
-        v.goForward();
-      //reload/stop
-      else if(s.value.match(/^r */))
-        v.isLoading()?v.stop():v.reload();
-      //load http-prefixed page
-      else if(x=s.value.match(/^h +(.+)/))
-        v.src='http://'+x[1];
-      //load any page (no auto-prefixing)
-      else if(x=s.value.match(/^H +(.+)/))
-        v.src=x[1];
-      //find in page
-      else if(x=s.value.match(/^f(c?) +(.+)/))
-        v.findInPage(x[2],{matchCase:!!x[1]});
-      //print
-      else if(s.value.match(/^p */))
-        v.print();
-      //search - you can change this to engine of your choice
-      else if(x=s.value.match(/^s +(.+)/))
-        v.loadURL('https://google.com/search?q='+encodeURIComponent(x[1]));
-      //new tab
-      else if(s.value.match(/^n */))
-        nV(),
-        v=V()[C=V().length-1],
-        v.addEventListener('did-finish-load',uT);
-      //left tab
-      else if(x=s.value.match(/^\[ */))
-        v=V()[C?--C:C],
-        uT();
-      //right tab
-      else if(x=s.value.match(/^] */))
-        v=V()[C<V().length-1?++C:C],
-        uT();
-      //close tab
-      else if((x=s.value.match(/^x */))&&V().length>1)
-        document.body.removeChild(v),
-        i.removeChild(document.querySelector('li.active')),
-        v=V()[C?--C:C],
-        uT();
+      //multiple commands can be separated by ;
+      (f=s=>{
+        S=s[0]
+        //devtools
+        if(S.match(/^d */))
+          v.openDevTools();
+        //go back
+        else if(S.match(/^< */))
+          v.goBack();
+        //go forward
+        else if(S.match(/^> */))
+          v.goForward();
+        //reload/stop
+        else if(S.match(/^r */))
+          v.isLoading()?v.stop():v.reload();
+        //load http-prefixed page
+        else if(x=S.match(/^h +(.+)/))
+          v.src='http://'+x[1];
+        //load any page (no auto-prefixing)
+        else if(x=S.match(/^H +(.+)/))
+          v.src=x[1];
+        //find in page
+        else if(x=S.match(/^f(c?) +(.+)/))
+          v.findInPage(x[2],{matchCase:!!x[1]});
+        //print
+        else if(S.match(/^p */))
+          v.print();
+        //search - you can change this to engine of your choice
+        else if(x=S.match(/^s +(.+)/))
+          v.loadURL('https://google.com/search?q='+encodeURIComponent(x[1]));
+        //new tab
+        else if(S.match(/^n */))
+          nV(),
+          v=V()[C=V().length-1],
+          v.addEventListener('did-finish-load',uT);
+        //left tab
+        else if(x=S.match(/^\[ */))
+          v=V()[C?--C:C],
+          uT();
+        //right tab
+        else if(x=S.match(/^] */))
+          v=V()[C<V().length-1?++C:C],
+          uT();
+        //close tab
+        else if((x=S.match(/^x */))&&V().length>1)
+          document.body.removeChild(v),
+          i.removeChild(document.querySelector('li.active')),
+          v=V()[C?--C:C],
+          uT();
+        //page url
+        else if(S.match(/u/))
+          alert(v.getURL());
+        //go onto next command if there is one
+        v.addEventListener('dom-ready',_=>{
+          s.shift()
+          s.length&&f(s)
+        })
+      })(s.value.split(/; */g))
 
       //reset command input value
       s.value=''
